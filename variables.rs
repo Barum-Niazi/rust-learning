fefb9afe7eb9f000a450c725e12dd1f3f678aa21
Barum Niazi <barumkhan123@gmail.com>
2025-09-16 02:39:43 +0500

    arrays

--- variables.rs
+++ variables.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let mut i = 0;
-    while i <= 5 {
-        print!("{i}\n");
-        if i == 1 {
-            i += 1;
-            continue;
-        }
-        if i == 2 {
-            break;
-        }
-        i += 1;
+    let mut array: [i32; 5] = [5, 4, 3, 2, 1];
+    let array_iterator = array.iter();
+    for item in array_iterator {
+        println!("item {}", item);
     }
+    for i in 0..5 {
+        array[i] = i as i32;
+        println!("item {}", array[i]);
+    }
+
+    println!("Rust slicing below");
+    println!("Slice = {:?}", &array[1..3]);
 }
