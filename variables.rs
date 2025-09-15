e7bf9b4247090c9dfd2c89fe621a89e5b1c2aa7b
Barum Niazi <barumkhan123@gmail.com>
2025-09-16 02:23:17 +0500

    Update variables.rs

--- variables.rs
+++ variables.rs
@@ -1,6 +1,14 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 10;
-    println!("The value of x is: {x}");
+    let mut i = 0;
+    while i <= 5 {
+        print!("{i}\n");
+        if i == 1 {
+            i += 1;
+            continue;
+        }
+        if i == 2 {
+            break;
+        }
+        i += 1;
+    }
 }
